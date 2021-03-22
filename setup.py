cf464d32fa3fcc4d77d17fe26eb154cb583bd352
alec-tschantz <tschantz.alec@gmail.com>
2021-03-22 10:38:30 +0000

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="pybrid",
+    version="0.0.1",
+    description=("Hybrid inference: Inferring fast and slow"),
+    license="MIT License",
+    url="https://github.com/alec-tschantz/pybrid/",
+    packages=[
+        "pybrid"
+    ],
+)
